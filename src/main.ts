f79ed0eb9d52776205839cf03f2df57c70471963
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-10-04 14:31:42 +0100

    mcv-added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { Options } from '@nestjs/common';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices"
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ["pkc-3w22w.us-central1.gcp.confluent.cloud:9092"],
+          ssl: true,
+          sasl: {
+            mechanism: 'plain',
+            username:'624CZI36XY2P3KTC',
+            password:'D4hzZpiFcgLSYmA8ykcPed5TviJQvwdScyxOU7RDig5z+PVVIm/dSTmbURMdDSzK',
+          }
+        }
+      }
+    }
+  );
+  app.listen(()=> console.log("app is listening ..."));
 }
 bootstrap();
+
